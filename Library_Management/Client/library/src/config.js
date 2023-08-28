47cff2db5177b3df909f79dc170971d91d36b9a9
65275_suraj <surajshinde1002@gmail.com>
2023-08-28 19:28:07 +0530

    deleted data

--- Library_Management/Client/library/src/config.js
+++
@@ -1,2 +0,0 @@
-export const URL = 'http://localhost:4000'
-export const SPRING_URL = 'http://localhost:8080'
--- Library_Management/Client/library/src/utils.js
+++
@@ -1,5 +0,0 @@
-import moment from 'moment-timezone'
-
-export const formatDate = (date) => {
-  return moment(date).utc().tz('Asia/Kolkata').format('MMM DD, YYYY')
-} 
